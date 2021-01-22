314ebc5555d57291f69d273b34663bfd6f07e2a8
Ben Wertenberger <bwertenberger52@gmail.com>
2021-01-22 14:37:29 -0600

    Debugging

--- 2D_environment.py
+++ 2D_environment.py
@@ -1,17 +1,17 @@
 import numpy as np
 import random
 count =0
-environment_matrix = np.array([[[None,0,-100,None],
+environment_matrix = np.array([[[None,0,0,None],
 								[None,0,0,0],
-								[None,None,-100,0]],
+								[None,None,-1000,0]],
 								
 								[[0,0,0,None],
-								[0,-100,0,-100],
-								[0,None,100,0]],
+								[0,-1000,0,0],
+								[0,None,1000,0]],
 								
-								[[-100,None,None,0],
-								[0,100,None,0],
-								[-100,0,None,None]]])
+								[[0,0,None,None],
+								[0,1000,None,0],
+								[-1000,None,None,0]]])
 q_matrix = np.array([[[0,0,0,0],
 					[0,0,0,0],
 					[0,0,0,0]],
@@ -28,19 +28,18 @@ win_loss_states = [[2,2],[0,1],[2,1]]
 
 def getAllPossibleNextAction(cur_x,cur_y):
 	action = []
-	step_matrix = [x != None for x in environment_matrix[cur_pos]]
-	if cur_y>0 and environment_matrix[cur_y,cur_x].ravel:
+	if cur_y>0 and type(environment_matrix[cur_y][cur_x][0])==type(1):
 		action.append(0)    
-	if cur_x<2 and environment_matrix[cur_y,cur_x].ravel:
+	if cur_x<2 and type(environment_matrix[cur_y][cur_x][1])==type(1):
 		action.append(1)
-	if cur_y<2 and environment_matrix[cur_y,cur_x].ravel:
+	if cur_y<2 and type(environment_matrix[cur_y][cur_x][2])==type(1):
 		action.append(2)
-	if cur_x>0 and environment_matrix[cur_y,cur_x].ravel:
+	if cur_x>0 and type(environment_matrix[cur_y][cur_x][3])==type(1):
 		action.append(3)
 	#else:
 		#again([cur_x,cur_y],action)
 		
-	return(action)
+	return action
 
 def isGoalStateReached(cur_pos):
 	return ([cur_x,cur_y] == [2,2])
@@ -56,16 +55,19 @@ def getNextState(cur_x, cur_y, action):
 		return [cur_x - 1,cur_y]
 		
 def isGameOver(cur_x, cur_y):
-	return [cur_x, cur_y] in win_loss_states
+	#return [cur_x, cur_y] in win_loss_states
+	for x in win_loss_states:
+		if [cur_x, cur_y] == x:
+			return True
 	
 #def again(cur_pos, action):
 	#getNextState(cur_pos[0], cur_pos[1], action)
 	
-discount = 0.9
-learning_rate = 0.5
-for _ in range(1000):
+discount = .7
+learning_rate = 1
+for _ in range(10000):
 	# get starting place
-	cur_pos = random.randrange(3),random.randrange(3)
+	cur_pos = [0,0]
 	# while goal state is not reached
 	while(not isGameOver(cur_pos[0],cur_pos[1])):
 		# get all possible next states from cur_step
@@ -75,11 +77,12 @@ for _ in range(1000):
 		# find the next state corresponding to the action selected
 		next_state = getNextState(cur_pos[0], cur_pos[1], action)
 		# update the q_matrix
-		if(environment_matrix[cur_pos[0],cur_pos[1]][action]):
-			q_matrix[cur_pos[0],cur_pos[1]][action] = x=q_matrix[cur_pos[0],cur_pos[1]][action] + learning_rate * (environment_matrix[cur_pos[0],cur_pos[1]][action] + discount * max(q_matrix[next_state].ravel()) - q_matrix[cur_pos[0], cur_pos[1]][action])
+		if(environment_matrix[cur_pos[0]][cur_pos[1]][action]):
+			q_matrix[cur_pos[1]][cur_pos[0]][action] = q_matrix[cur_pos[1]][cur_pos[0]][action] + learning_rate * (environment_matrix[cur_pos[1]][cur_pos[0]][action] + discount * max(q_matrix[next_state].ravel()) - q_matrix[cur_pos[1]][ cur_pos[0]][action])
 			count+=1
 		# go to next state
 		cur_pos = next_state
+		
 		
 	# print status
 	print("Episode ", _ , " done")
