399ee25ef7581ff4bb2683265c1a9f9ba4f746ec
Ben Wertenberger <bwertenberger52@gmail.com>
2021-02-10 16:43:49 -0600

    Updated Display and Reward.
    Rewards are now no longer meaningless and agents are taking actions.

--- Crisis_sim_environment.py
+++ Crisis_sim_environment.py
@@ -5,21 +5,23 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-environment_matrix = np.zeros((20,20,6))
+environment_matrix = np.zeros((20,20,6),dtype = float)
 
-q_matrix = np.zeros((20,20,6))
+q_matrix = np.zeros((20,20,6),dtype = float)
 
 pos_matrix = np.zeros((20,20),dtype = int)
-# ~ screen=turtle.getscreen()
-# ~ screen.tracer(0)
-# ~ turtle.up()
-# ~ turtle.ht()
-# ~ turtle.update()
-for x in range(random.randrange(20)):
-	environment_matrix[0][x][0]=environment_matrix[19][x][2]=environment_matrix[x][0][1]=environment_matrix[x][19][3] = None
 
+food_spots = []
+gathered = False
+for x in range(20):
+	environment_matrix[0][x][0]=environment_matrix[19][x][2]=environment_matrix[x][0][1]=environment_matrix[x][19][3] = None
+for i in range(8):
+	food_x = random.randrange(20)
+	food_y = random.randrange(20)
+	environment_matrix[food_y][food_x][5] = 50
+	food_spots.append([food_x,food_y,gathered])
 screen=turtle.getscreen()
-screen.tracer(1)
+screen.tracer(0)
 turtle.up()
 turtle.ht()
 turtle.update()
@@ -27,7 +29,6 @@ SUSCEPTIBLE=(0,0,1)
 INFECTED=(1,0,0)
 ZOMBIE=(0,1,0)
 RECOVERED=(.5,.5,.5)
-MOVE_DISTANCE=20
 X=0
 Y=1
 STATUS=2
@@ -65,34 +66,40 @@ def infectPopulation(population,n=1):
 			zombies.append(person)
 			n-=1
 
-def display(population):
+def display(population,food_spots):
 	
-    turtle.clear()
-    turtle.goto(220,220)
-    turtle.down()
-    turtle.seth(180)
-    
-    for i in range(4):
+	turtle.clear()
+	turtle.goto(220,220)
+	turtle.down()
+	turtle.seth(180)
+	
+	for i in range(4):
 		
-        turtle.forward(440)
-        turtle.left(90)
-        
-    turtle.up()
-    turtle.shapesize(2,2)
-    
-    for x,y,status in population:
+		turtle.forward(440)
+		turtle.left(90)
 		
-        turtle.goto(x,y)
-        turtle.fillcolor(status)
-        turtle.stamp()
-        
-    turtle.update()
+	turtle.up()
+ 
+	
+	for x,y,gathered in food_spots:
+		turtle.shapesize(1,1)
+		turtle.shape("square")
+		turtle.goto((x*20)-200,(y*20)-200)
+		turtle.fillcolor("yellow")
+		turtle.stamp()	
+ 	
+	for x,y,status in population:
+		turtle.shape("circle")
+		turtle.goto(x,y)
+		turtle.fillcolor(status)
+		turtle.stamp()
+		
+	turtle.update()
 	
 def step(population,infectionRadius,infectionRate):
 	for y in pos_matrix:
 		for x in pos_matrix:
-			print(pos_matrix[y][x])
-			if pos_matrix[y][x].all() == 1:
+			if pos_matrix[y][x].all== 1:
 				environment_matrix[y][x-1][1]=environment_matrix[y][x+1][3]=environment_matrix[y-1][x][2]=environment_matrix[y+1][x][0]=None
 	for person in population:
 		
@@ -100,25 +107,25 @@ def step(population,infectionRadius,infectionRate):
 		x_index = int((x+200)/20)
 		y_index = int((y+200)/20)
 		discount = 0.2
-		learning_rate = 0.5
+		learning_rate = .5
 		possible_actions = getAllPossibleNextAction(x_index,y_index)
 		action = random.choice(possible_actions)
 		next_state = getNextState(x_index, y_index, action)
 		q_matrix[y_index][x_index][action] = q_matrix[y_index][x_index][action] + learning_rate * (environment_matrix[y_index][x_index][action] + discount * max(q_matrix[next_state].ravel()) - q_matrix[y_index][x_index][action])
+		
 		person[X] = (next_state[0]*20)-200
 		person[Y] = (next_state[1]*20)-200
-		
 
 
 def getAllPossibleNextAction(cur_x,cur_y):
 	action = []
-	if cur_y>0 and type(environment_matrix[cur_y][cur_x][0])==int:
+	if cur_y>0 and type(environment_matrix[cur_y][cur_x][0])!=None:
 		action.append(0)    
-	if cur_x<19 and type(environment_matrix[cur_y][cur_x][1])==int:
+	if cur_x<19 and type(environment_matrix[cur_y][cur_x][1])!=None:
 		action.append(1)
-	if cur_y<19 and type(environment_matrix[cur_y][cur_x][2])==int:
+	if cur_y<19 and type(environment_matrix[cur_y][cur_x][2])!=None:
 		action.append(2)
-	if cur_x>0 and type(environment_matrix[cur_y][cur_x][3])==int:
+	if cur_x>0 and type(environment_matrix[cur_y][cur_x][3])!=None:
 		action.append(3)
 	action.append(4)
 	action.append(5)
@@ -134,12 +141,15 @@ def getNextState(cur_x, cur_y, action):
 		return [cur_x,cur_y + 1]
 	elif (action == 3):
 		return [cur_x - 1,cur_y]
-	elif (action == 4):
-		return [cur_x,cur_y]
-	elif (action == 5):
-		gather()
-		return [cur_x,cur_y]
+	elif (action == 4): 
+		#fight()
+		return [cur_x,cur_y] 
+	elif (action == 5): 
+		gather() 
+		return [cur_x,cur_y] 
 		
+def gather():
+	pass
 		
 def expose(person,population,infectionRadius,infectionRate):
 	
@@ -158,8 +168,6 @@ def expose(person,population,infectionRadius,infectionRate):
 				person[STATUS]=INFECTED
 				break
 				
-def gather():
-	pass
 	
 	
 	
@@ -203,7 +211,7 @@ def census(population):
 	return s,i,r,z
 
 def simulation(n=100,InitialInfectionCount=1,infectionRadius=20,infectionRate=.02,show=True):
-	
+	count = 0 
 	population=getPopulation(n)
 	infectPopulation(population,n=InitialInfectionCount)
 	s,i,r,z=census(population)
@@ -211,7 +219,6 @@ def simulation(n=100,InitialInfectionCount=1,infectionRadius=20,infectionRate=.0
 	I=[i]
 	R=[r]
 	Z=[z]
-	
 	while (True):
 		
 		step(population,infectionRadius,infectionRate)
@@ -223,9 +230,12 @@ def simulation(n=100,InitialInfectionCount=1,infectionRadius=20,infectionRate=.0
 		infectionRate*=(.55+random.random())
 		
 		if show:
-			display(population)
-			
-		if(((S[-1]==0) and (I[-1]==0)) or ((Z[-1]==0) and (I[-1]==0))):
+			display(population,food_spots)
+		count += 1
+		#if(((S[-1]==0) and (I[-1]==0)) or ((Z[-1]==0) and (I[-1]==0))):
+		if count == 1000:
+			print('Done')
+			print(q_matrix.tolist())
 			break
 			
 	return S,I,R,Z
